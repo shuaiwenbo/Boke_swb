2fa7ffb27f6ec202ca1d915329639aabed4c754d
shuaiwenbo <shaiwenbo@csii.com.cn>
2019-05-02 20:05:26 +0800

    首次提交

---
+++ boke-demo/src/main/java/com/csii/boke/AppStartApplication.java
@@ -0,0 +1,14 @@
+package com.csii.boke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author 86130
+ *
+ */
+@SpringBootApplication
+public class AppStartApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AppStartApplication.class, args);
+	}
+}
