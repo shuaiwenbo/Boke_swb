6efe945f7f38a41a8a8163f3cfcfce0b18f37970
shuaiwenbo <shaiwenbo@csii.com.cn>
2019-05-02 20:52:57 +0800

    测试idea提交

--- boke-demo/src/main/java/com/csii/boke/AppStartApplication.java
+++ boke-demo/src/main/java/com/csii/boke/AppStartApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppStartApplication {
 	public static void main(String[] args) {
+		System.out.print("test idea push ");
 		SpringApplication.run(AppStartApplication.class, args);
 	}
 }
