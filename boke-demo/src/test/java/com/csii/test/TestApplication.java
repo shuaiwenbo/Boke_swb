30934d1b5cb6b2b32582d1981f41610f030f9dcc
shuaiwenbo <shaiwenbo@csii.com.cn>
2019-05-03 22:03:32 +0800

    添加application.properties文件

--- boke-demo/src/test/java/com/csii/test/TestApplication.java
+++ boke-demo/src/test/java/com/csii/test/TestApplication.java
@@ -4,13 +4,16 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration
 public class TestApplication {
 	@Autowired
 	DataSource dataSource;
@@ -19,5 +22,6 @@ public class TestApplication {
 		System.out.println(dataSource.getClass());
 		Connection connection=dataSource.getConnection();
 		System.out.println(connection);
+		System.out.println("===============================================");
 	}
 }
