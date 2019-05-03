af24978a5c67a3ec4973c35094ee63510369c555
shuaiwenbo <shaiwenbo@csii.com.cn>
2019-05-03 21:10:24 +0800

    测试datasource

--- boke-demo/src/test/java/com/csii/test/Test.java
+++ boke-demo/src/test/java/com/csii/test/Test.java
@@ -1,5 +1,22 @@
 package com.csii.test;
 
-public class Test {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class Test {
+	@Autowired
+	DataSource dataSource;
+	@org.junit.Test
+	public void contextLoads() throws SQLException {
+		System.out.println(dataSource.getClass());
+		Connection connection=dataSource.getConnection();
+		System.out.println(connection);
+	}
 }
