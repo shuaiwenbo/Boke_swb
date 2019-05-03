a5879dbdc84da6120c89453e304df362bb4bc447
shuaiwenbo <shaiwenbo@csii.com.cn>
2019-05-03 21:15:47 +0800

    添加application.properties文件

--- boke-demo/src/main/java/com/csii/boke/AppStartApplication.java
+++ boke-demo/src/main/java/com/csii/boke/AppStartApplication.java
@@ -2,6 +2,9 @@ package com.csii.boke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
 /**
  * @author 86130
  *
